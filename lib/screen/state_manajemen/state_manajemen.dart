caa4b52b68c52c5c3fb7898958dad8520d4bb10c
Azlan-saja <azlansaja19@gmail.com>
2025-05-14 12:25:08 +0700

    p12

--- lib/screen/state_manajemen/state_manajemen.dart
+++ lib/screen/state_manajemen/state_manajemen.dart
@@ -15,24 +15,50 @@ class _StateManajemenState extends State<StateManajemen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Layar State Manajemen')),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    valueCounter--; //valueCounter = valueCounter-1;
-                  });
-                },
-                child: const Text('-1')),
-            Text('$valueCounter', style: const TextStyle(fontSize: 37)),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    valueCounter++;
-                  });
-                },
-                child: const Text('+1')),
+            const Text(
+              'Local State: SetState()',
+              style: TextStyle(fontSize: 18),
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        valueCounter--;
+                      });
+                    },
+                    child: const Text('-1')),
+                Text('$valueCounter', style: const TextStyle(fontSize: 37)),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        valueCounter++;
+                      });
+                    },
+                    child: const Text('+1')),
+              ],
+            ),
+            const Divider(),
+            const SizedBox(height: 60),
+            const Text(
+              'Global State: Package Provider',
+              style: TextStyle(fontSize: 18),
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text('-1')),
+                const Text('X', style: TextStyle(fontSize: 37)),
+                ElevatedButton(onPressed: () {}, child: const Text('+1')),
+              ],
+            ),
+            const Divider(),
           ],
         ),
       ),
